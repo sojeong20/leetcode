5f27c6450d19b472f4556678e58d8f153ed1e2f4
sojeong20 <69193892+sojeong20@users.noreply.github.com>
2023-05-29 17:33:02 +0900

    Time: 8134 ms (5.80%), Space: 20.9 MB (14.51%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,7 +4,6 @@ class Solution:
         nums.sort()
 
         for i in range(len(nums) - 2):
-
             left, right = i + 1, len(nums) - 1
             while left < right:
                 sum = nums[i] + nums[left] + nums[right]
@@ -16,9 +15,9 @@ class Solution:
                     triplets = [nums[i], nums[left], nums[right]]
                     if triplets not in result:
                         result.append(triplets)
-                    
+				
+                    # 현재는 SUM이 0이므로 포인터를 둘 다 이동한다.
                     left += 1
                     right -= 1
-                    
+                
         return result
-		
