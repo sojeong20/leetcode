aebb233adb2785cd0f7d2e93e0ceffefc917d153
sojeong20 <69193892+sojeong20@users.noreply.github.com>
2023-09-17 16:46:53 +0900

    Time: 108 ms (47.21%), Space: 16.6 MB (81.31%) - LeetHub

--- 0393-utf-8-validation/0393-utf-8-validation.py
+++ 0393-utf-8-validation/0393-utf-8-validation.py
@@ -1,35 +1,23 @@
 class Solution:
     def validUtf8(self, data: List[int]) -> bool:
-        data = [bin(d)[2:].zfill(8) for d in data]
-        start = 0
-        
-        def get_length(c: str):
-            if c[:5] == '11110':
-                return 4
-            elif c[:4] == '1110':
-                return 3
-            elif c[:3] == '110':
-                return 2
-            else:
-                return 1
-        
-        def check_followed_character(c: str):
-            return c[:2] == '10'
-
-
-        while start < len(data):
-            length = get_length(data[start])
-            if length > len(data):
-                return False
-            
-            if length > 1:
-                for c in data[start+1:start+length]:
-                    if not check_followed_character(c):
-                        return False
-            else:
-                if data[start][0] != '0':
+        def check(size):
+            for i in range(start+1, start+size+1):
+                if i >= len(data) or (data[i] >> 6) != 0b10:
                     return False
-                
-            start += length
+            return True
+
+        start = 0
+        while start < len(data):
+            first = data[start]
+            if (first >> 3) == 0b11110 and check(3):
+                start += 4
+            elif (first >> 4) == 0b1110 and check(2):
+                start += 3
+            elif (first >> 5) == 0b110 and check(1):
+                start += 2
+            elif (first >> 7) == 0:
+                start += 1
+            else:
+                return False
 
         return True
