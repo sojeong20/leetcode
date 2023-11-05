ec995bd6399b0e961991d38ef6dca67b988a23a9
sojeong20 <69193892+sojeong20@users.noreply.github.com>
2023-11-06 00:07:16 +0900

    Time: 33 ms (88.29%), Space: 16.1 MB (92.34%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,14 +1,8 @@
-import collections
-
 class Solution:
-    dp = collections.defaultdict(int)
-    
     def fib(self, n: int) -> int:
-        self.dp[0] = 0
-        self.dp[1] = 1
-
-        for i in range(2, n+1):
-            self.dp[i] = self.dp[i-1] + self.dp[i-2]
-            
-        return self.dp[n]
+        x, y = 0, 1
         
+        for i in range(0, n):
+            x, y = y, x + y
+            
+        return x
