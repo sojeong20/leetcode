7fd9151b3cc30dcf5fe5876b0f0e4d6ef3ad3c28
sojeong20 <69193892+sojeong20@users.noreply.github.com>
2023-11-06 14:50:50 +0900

    Time: 37 ms (73.27%), Space: 16.1 MB (95.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -7,9 +7,8 @@ class Solution:
         if len(nums) <= 2:
             return max(nums)
         
-        max_money = []
-        max_money.append(nums[0])
-        max_money.append(max(nums[0], nums[1]))
+        max_money = [nums[0], max(nums[0], nums[1])]
+
         for i in range(2, len(nums)):
             max_money.append(max(max_money[i-2] + nums[i], max_money[i-1]))
             
