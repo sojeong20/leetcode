b32d03fd7de9fd681b48d4448e5fcbd204c265b5
sojeong20 <69193892+sojeong20@users.noreply.github.com>
2024-01-04 22:25:33 +0900

    Time: 125 ms (31.81%), Space: 18.3 MB (15.80%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,16 +1,10 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         for k, v in enumerate(numbers):
-            left, right = k + 1, len(numbers) - 1
             expected = target - v
             
-            while left <= right:
-                mid = left + (right - left) // 2
-                if numbers[mid] < expected:
-                    left = mid + 1
-                elif numbers[mid] > expected:
-                    right = mid - 1
-                else:
-                    return k + 1, mid + 1
-            
+            i = bisect.bisect_left(numbers, expected, k + 1)
+            if i < len(numbers) and numbers[i] == expected:
+                return k + 1, i + 1
+
  
